3b3fb9f86cead76b396613a048eeb8778ef177e8
Dmitrii <ruslanus866@outlook.com>
2020-12-01 20:02:39 +0300

    remove file downloader and merge upload and download in one file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import FileUpload from "./FileUpload";
-import FileDownloader from "./FileDownloader";
 
 function App() {
   return (
       <div className="App">
           <FileUpload />
-          <FileDownloader />
       </div>
   );
 }
--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,12 +1,33 @@
 import React, { useState, useRef } from "react";
 import axios, { CancelToken, isCancel } from "axios";
 import { ProgressBar } from "react-bootstrap";
+import useFileDownloader from "./hooks/useFileDownloader";
+
+const file_detail = {
+    name: "Photo 1",
+    thumb: "/rs/identity/image/Q8007",
+    file: "/rs/identity/image/Q8007",
+    filename: "photo-1.jpg",
+};
 
 const FileUpload = () => {
     const [uploadPercentage, setUploadPercentage] = useState(0);
     const cancelFileUpload = useRef(null);
+    const [image, setImage] = useState({ file: "", imagePreviewUrl: "" });
+    const [downloadFile, downloaderComponentUI] = useFileDownloader();
 
     const uploadFile = ({ target: { files } }) => {
+        let reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImage({
+                file: files[0],
+                imagePreviewUrl: reader.result,
+            });
+        };
+
+        reader.readAsDataURL(files[0]);
+
         let data = new FormData();
         data.append("file", files[0]);
 
@@ -31,9 +52,9 @@ const FileUpload = () => {
                 console.log(res);
                 setUploadPercentage(100);
 
-                setTimeout(() => {
-                    setUploadPercentage(0);
-                }, 1000);
+                // setTimeout(() => {
+                //     setUploadPercentage(0);
+                // }, 3000);
             })
             .catch((err) => {
                 console.log(err);
@@ -50,9 +71,10 @@ const FileUpload = () => {
             cancelFileUpload.current("User has canceled the file upload.");
     };
 
+    const download = (file) => downloadFile(file);
     return (
         <>
-            <div className="row justify-content-center bg-light">
+            <div className="row justify-content-center bg-light p-5">
                 <div className="col-md-6 text-center">
                     <h2>Upload your profile picture</h2>
 
@@ -84,8 +106,37 @@ const FileUpload = () => {
                                     Cancel
                                 </span>
                             </div>
+                            <div className="col">
+                                <label htmlFor="upload-button">
+                                    {image.imagePreviewUrl ? (
+                                        <img
+                                            src={image.imagePreviewUrl}
+                                            alt="dummy"
+                                            width="300"
+                                            height="300"
+                                        />
+                                    ) : (
+                                        <>
+                                            <span className="fa-stack fa-2x mt-3 mb-2">
+                                                <i className="fas fa-circle fa-stack-2x" />
+                                                <i className="fas fa-store fa-stack-1x fa-inverse" />
+                                            </span>
+                                            <h5 className="text-center">
+                                                Upload your photo
+                                            </h5>
+                                        </>
+                                    )}
+                                </label>
+                            </div>
                         </div>
                     )}
+                    <button
+                        className="btn btn-primary cursor-pointer text-white"
+                        onClick={() => download(file_detail)}
+                    >
+                        Download{" "}
+                    </button>
+                    {downloaderComponentUI}
                 </div>
             </div>
         </>
