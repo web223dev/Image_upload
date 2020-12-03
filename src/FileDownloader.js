cbd8e00c681d3e4a8e345acc4d526e45e6c4d9ea
Dmitrii <ruslanus866@outlook.com>
2020-12-03 03:59:37 +0300

    changed download to GET endpoint

--- src/FileDownloader.js
+++ src/FileDownloader.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import axios, { isCancel } from "axios";
 import useFileDownloader from "./hooks/useFileDownloader";
 
-const file = {
-    name: "Photo 1",
-    thumb: "/rs/identity/image/Q8007",
-    file: "/rs/identity/image/Q8007",
-    filename: "photo-1.jpg",
-};
+function downloadImage(img) {
+  const downloadLink = document.createElement("a");
+  const fileName = "photo-1.jpg";
+
+  downloadLink.href = img;
+  downloadLink.download = fileName;
+  downloadLink.click();
+}
 
 const FileDownloader = () => {
     const [downloadFile, downloaderComponentUI] = useFileDownloader();
+    const [ImgSource, setImageSource] = useState(null);
 
-    const download = (file) => downloadFile(file);
+    axios
+      .get(
+        '/rs/identity/image/Q8007',
+        { responseType: 'arraybuffer' },
+      )
+      .then(response => {
+        const base64 = btoa(
+          new Uint8Array(response.data).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            '',
+          ),
+        );
+        setImageSource("data:;base64," + base64);
+    });
 
+    
+
+    // const download = (file) => downloadFile(file); 
     return (
         <>
             <div className="row mt-5">
@@ -24,16 +44,16 @@ const FileDownloader = () => {
                                 <div className="card-body">
                                     <img
                                         className="card-img-top mb-3"
-                                        src={file.thumb}
+                                        src={ImgSource}
                                     />
-                                    <h5 className="card-title">{file.name}</h5>
+                                    <h5 className="card-title">Photo 1</h5>
 
-                                    <a
+                                    <button
                                         className="btn btn-primary cursor-pointer text-white"
-                                        onClick={() => download(file)}
+                                        onClick={()=>downloadImage(ImgSource)}
                                     >
                                         Download{" "}
-                                    </a>
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -43,6 +63,6 @@ const FileDownloader = () => {
             </div>
         </>
     );
-};
+}
 
 export default FileDownloader;
